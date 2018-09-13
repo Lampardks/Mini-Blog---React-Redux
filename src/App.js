09ee47b1d62376c1a50133bd5ebb70bb927984c4
Denis Romanov <lampard.ks@gmail.com>
2018-09-13 22:58:04 +0300

    Add code splitting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import { hot } from "react-hot-loader";
+import Loadable from "react-loadable";
+import { Loader } from "semantic-ui-react";
 
-import Home from "./components/pages/HomePage/HomePage";
-import Article from "./components/pages/ArticlePage/ArticlePage";
 import TopNavigation from "./components/navigation/TopNavigation";
 
 import "./App.css";
+
+const Loading = () => <Loader active />;
+
+const Home = Loadable({
+  loader: () => import("./components/pages/HomePage/HomePage"),
+  loading: Loading,
+});
+
+const Article = Loadable({
+  loader: () => import("./components/pages/ArticlePage/ArticlePage"),
+  loading: Loading,
+});
 
 const App = () => {
   return (
